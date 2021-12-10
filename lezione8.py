808d07c56bed67602a6ac5490a41934529236016
Roberto <94189466+Roberto-Coppola@users.noreply.github.com>
2021-12-10 16:39:52 +0000

    programma lezione 8 fatto

--- lezione8.py
+++ lezione8.py
@@ -1,16 +1,20 @@
-class model()
+class model():
     def predict(self, data):
         raise NotImplementedError('Metodo non implementato')
-class IncrementModel(model)
+class IncrementModel(model):
     def predict(self, data):
         prev_value=None
-        n=len(data)
+        n=len(data)-1
         tot=0
+        var=0
         for item in data:
-            var=1
-            tot=tot+item[var]-item[var-1]
-            var=var+1
+            if var==0:
+                var=var+1
+            else:
+                tot=tot+item-data[var-1]
+                var=var+1
         predict=tot/n
-        valore_incrementato=data[n-1]+IncrementModel
+        valore_incrementato=data[n]+predict
+        return valore_incrementato
 dati=[50, 52, 60]
-print('presdizione {}'. format(IncrementModel().predict(dati)))
+print('predizione {}'. format(IncrementModel().predict(dati)))
